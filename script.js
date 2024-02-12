4d1eb3bddf232021c827c50c72f4119f02a548da
Kenneth Antwi <antwikenneth14@gmail.com>
2024-02-12 13:54:21 +0000

    working on Appy Navbar

---
+++ script.js
@@ -0,0 +1,20 @@
+//alert('we are beta!!!');
+console.log('we are beta!!!');
+
+var firstName = 'Kenneth' ;
+var age = 0;
+
+let lastName = 'Antwi';
+const boilingWaterTemperature = 100;
+
+
+console.log(firstName, age, lastName, boilingWaterTemperature );
+if ( age >=18){
+    console.log('welcome to My page ');
+} else {
+    console.log('this page is not for you ');
+}
+
+for (let i = 1; i != 8; i++ ) {
+    console.log('the value of i', i );
+}
